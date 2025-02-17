782790857626d33be42790f05b570bb06e7e4ba8
Fábio Luiz Nery de Miranda <fabio@7gen.com>
2025-02-17 12:28:22 -0800

    Improve User toJSON serialization

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -21,6 +21,15 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true
   }
+}, {
+  toJSON: {
+    transform(doc, ret) {
+      ret.id = ret._id
+      delete ret._id
+      delete ret.__v
+      delete ret.password
+    }
+  }
 })
 
 userSchema.statics.build = (attrs: UserAttrs) => {
