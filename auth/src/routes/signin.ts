913e084eea5bd87f1748873e63e86c93a232ca76
Fábio Luiz Nery de Miranda <fabio@7gen.com>
2025-02-17 12:47:44 -0800

    Moved validateRequest to a middleware

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,26 +1,25 @@
 import express, { Request, Response } from 'express'
-import { body, validationResult } from 'express-validator'
-import { RequestValidationError } from '../errors/request-validation-error'
+import { body } from 'express-validator'
+import { validateRequest } from '../middlewares/validate-request'
 
 const router = express.Router()
 
-router.post('/api/users/signin', [
-  // validation
-  body('email')
-    .isEmail()
-    .withMessage('Email must be valid'),
-  body('password')
-    .trim()
-    .notEmpty()
-    .withMessage('You must supply a password')
-], (req: Request, res: Response) => {
-  const errors = validationResult(req)
-
-  if (!errors.isEmpty()) {
-    throw new RequestValidationError(errors.array())
+router.post(
+  '/api/users/signin',
+  [
+    // validation
+    body('email')
+      .isEmail()
+      .withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .notEmpty()
+      .withMessage('You must supply a password')
+  ],
+  validateRequest,
+  (req: Request, res: Response) => {
+    res.send('Sign in')
   }
-
-  res.send('Sign in')
-})
+)
 
 export { router as signinRouter }
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,43 +1,42 @@
 import express, { Request, Response } from 'express'
-import { body, validationResult } from 'express-validator'
+import { body } from 'express-validator'
 import jwt from 'jsonwebtoken'
 
-import { RequestValidationError } from '../errors/request-validation-error'
 import { User } from '../models/user'
 import { BadRequestError } from '../errors/bad-request-error'
+import { validateRequest } from '../middlewares/validate-request'
 
 const router = express.Router()
 
-router.post('/api/users/signup', [
-  body('email')
-    .isEmail()
-    .withMessage('Email must be valid'),
-  body('password')
-    .trim()
-    .isLength({ min: 4, max: 20 })
-    .withMessage('Password must be between 8 and 20 characters')
-], async (req: Request, res: Response) => {
-  const errors = validationResult(req)
+router.post(
+  '/api/users/signup',
+  [
+    body('email')
+      .isEmail()
+      .withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage('Password must be between 8 and 20 characters')
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body
 
-  if (!errors.isEmpty()) {
-    throw new RequestValidationError(errors.array())
+    const existingUser = await User.findOne({ email })
+
+    if (existingUser) {
+      throw new BadRequestError('Email in use')
+    }
+
+    const user = User.build({ email, password })
+    await user.save()
+
+    const { id } = user
+    req.session = { jwt: jwt.sign({ id, email }, process.env.JWT_KEY!) }
+
+    res.status(201).send(user)
   }
-
-  const { email, password } = req.body
-
-  const existingUser = await User.findOne({ email })
-
-  if (existingUser) {
-    throw new BadRequestError('Email in use')
-  }
-
-  const user = User.build({ email, password })
-  await user.save()
-
-  const { id } = user
-  req.session = { jwt: jwt.sign({ id, email }, process.env.JWT_KEY!) }
-
-  res.status(201).send(user)
-})
+)
 
 export { router as signupRouter }
