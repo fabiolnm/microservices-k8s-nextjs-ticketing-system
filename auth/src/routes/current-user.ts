8565c9719ffa8d2c96c6a2ea7ec7724f84d3c34b
Fábio Luiz Nery de Miranda <fabio@7gen.com>
2025-02-17 17:13:42 -0800

    Signin route

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,19 @@
 import express from 'express'
+import jwt from 'jsonwebtoken'
 
 const router = express.Router()
 
 router.get('/api/users/currentuser', (req, res) => {
-  res.send('Current User')
+  try {
+    if (req.session?.jwt) {
+      const currentUser = jwt.verify(req.session.jwt, process.env.JWT_KEY!)
+      res.send({ currentUser })
+      return
+    }
+  } catch (err) {
+    // ignore and return currentUser: null
+  }
+  res.send({ currentUser: null })
 })
 
 export { router as currentUserRouter }
