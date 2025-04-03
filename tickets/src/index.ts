ad75d1c67c4ad044b1e360b690dff9967af9863e
Fábio Luiz Nery de Miranda <fabio@7gen.com>
2025-04-03 11:37:01 -0700

    Extract tickets/NATS env vars

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -9,8 +9,14 @@ async function start() {
   if (!process.env.MONGO_URI) {
     throw new Error('MONGO_URI must be defined')
   }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS_CLUSTER_ID must be defined')
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error('NATS_URL must be defined')
+  }
   try {
-    await natsWrapper.connect('ticket', 'nats-srv:4222')
+    await natsWrapper.connect(process.env.NATS_CLUSTER_ID, process.env.NATS_URL)
     await mongoose.connect(process.env.MONGO_URI)
     console.log('Connected to MongoDB')
   } catch (err) {
